0ea35e4f4a821194604200e07376bdd1238cc272
Aswincv <aswin.cv@mountblue.io>
2019-02-05 11:42:03 +0530

    made changes in the model

--- management/models.py
+++ management/models.py
@@ -4,55 +4,49 @@ import datetime
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+
 class Shop(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50, blank=True, null=True)
+    name = models.CharField(unique=True, max_length=50, blank=True, null=True)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=50, blank=True, null=True)
 
-# class Product(models.Model):
-#     name = models.CharField(max_length=50, blank=True, null=True)
-    # category = models.ForeignKey('Category', models.DO_NOTHING, blank=True, null=True)
 
 class Supplier(models.Model):
     supplier = models.OneToOneField(User, on_delete=models.CASCADE)
     contact_number = PhoneNumberField(null=False, blank=False, unique=True)
 
-class Item(models.Model):
-    # product = models.ForeignKey('Product', models.DO_NOTHING, blank=True, null=True)
-    name = models.CharField(max_length=50, blank=True, null=True)
-    category = models.ForeignKey('Category', models.DO_NOTHING, blank=True, null=True)
-    shop = models.ForeignKey('Shop', models.DO_NOTHING, blank=True, null=True)
+
+class Product(models.Model):
+    name = models.CharField(unique=True, max_length=50, blank=True, null=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    shop = models.ForeignKey('Shop', on_delete=models.CASCADE)
     mfg_date = models.DateField(("Date"), default=timezone.now)
     exp_date = models.DateField(("Date"), default=timezone.now)
     buying_price = models.FloatField(null=True, blank=True, default=None)
     selling_price = models.FloatField(null=True, blank=True, default=None)
     quantity_reamins = models.FloatField(null=True, blank=True, default=None)
-    supplier = models.ForeignKey('Supplier', models.DO_NOTHING, blank=True, null=True)
+    supplier = models.ForeignKey('Supplier', on_delete=models.CASCADE)
 
 
 class Customer(models.Model):
     customer = models.OneToOneField(User, on_delete=models.CASCADE)
 
+
 class Bill(models.Model):
-    customer = models.ForeignKey('Customer', models.DO_NOTHING, blank=True, null=True)
-    bill_item = models.ForeignKey('BillItem', models.DO_NOTHING, blank=True, null=True)
+    shop = models.ForeignKey('Shop', on_delete=models.CASCADE)
+    customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
 class BillItem(models.Model):
-    Item = models.ForeignKey('Item', models.DO_NOTHING, blank=True, null=True)
+    bill = models.ForeignKey(Bill, on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
     selling_price = models.FloatField(null=True, blank=True, default=None)
     quantity = models.FloatField(null=True, blank=True, default=None)
-    total_price = models.FloatField(null=True, blank=True, default=None)
 
-class CustomerBill(models.Model):
-    date = models.DateField(("Date"), default=timezone.now)
-    customer = models.OneToOneField(Customer, on_delete=models.CASCADE)
-    bill = models.ForeignKey('Bill', models.DO_NOTHING, blank=True, null=True)
-    shop = models.ForeignKey('Shop', models.DO_NOTHING, blank=True, null=True)
-    total_price = models.FloatField(null=True, blank=True, default=None)
-
-class BillHistory(models.Model):
-    owner = models.OneToOneField(User, on_delete=models.CASCADE)
-    shop = models.ForeignKey('Shop', models.DO_NOTHING, blank=True, null=True)
-    cutomerbill = models.ForeignKey('BillHistory', models.DO_NOTHING, blank=True, null=True)
+    @property
+    def total_price(self):
+        return self.selling_price * self.quantity
